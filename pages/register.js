47bbd56c9b59685c7b0908819a7ce0c49b87a690
desiretillerasRS <desire.tilleras@rssoftware.se>
2022-03-08 11:19:21 +0100

    List all registered

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,38 @@
-import Form from '../components/form.js'
+import Form from "../components/form.js";
 
-const register =()=>{
-
-    return(
-        <div>
-            <h1>Register</h1>
-            <Form/>
-        </div>
-    )
-
+function register({ names }) {
+  console.log(names.Names);
+  return (
+    <div>
+      <Form />
+      <p>Anmälda:</p>
+      <ul>
+        {names.Names.map((name) => (
+          <li>{name.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default register;
+
+export async function getServerSideProps(context) {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch("http://localhost:3000/api/allRegistered");
+  const names = await res.json();
+
+  if (!names) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: { names },
+  };
+}
